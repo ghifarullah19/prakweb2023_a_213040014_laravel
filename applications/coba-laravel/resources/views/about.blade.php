e3237728ca8bf7d5a8cc382576cbd39426575077
Muhammad Lutfi Amin Ghifarullah <ghifarullah11@gmail.com>
2023-11-03 10:25:43 +0700

    5. Model, Collection & Controller

--- applications/coba-laravel/resources/views/about.blade.php
+++ applications/coba-laravel/resources/views/about.blade.php
@@ -5,5 +5,5 @@
     <h3>{{ $name }}</h3>
     <p>{{ $email }}</p>
     <img src="img/{{ $image }}" alt="{{ $name }}" width="
-    200">    
+    200" class="img-thumbnail rounded-circle">    
 @endsection
--- applications/coba-laravel/routes/web.php
+++ applications/coba-laravel/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,58 +32,5 @@ Route::get('/about', function () {
     ]);
 });
 
-
-
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            'title' => "Judul Post Pertama",
-            'slug' => "judul-post-pertama",
-            'author' => "Muhammad Lutfi",
-            'body' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur a illo ipsam commodi tenetur rerum cupiditate. Exercitationem, labore voluptatum ipsum cum excepturi dolor iure sint consequatur numquam libero ex laudantium sit ipsa esse nisi, dolorum expedita possimus beatae, maxime veniam a pariatur eos impedit. Incidunt mollitia laboriosam dolorum odio harum ratione, quo, quod quibusdam expedita asperiores fugit exercitationem voluptatum ex rerum neque, suscipit at eveniet corrupti repellat accusamus dicta! Sequi accusamus voluptatum voluptate, aut animi provident laboriosam molestias incidunt ullam."
-        ],
-        [
-            'title' => "Judul Post Kedua",
-            'slug' => "judul-post-kedua",
-            'author' => "Fauzi Kirito",
-            'body' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde provident aperiam ut vero odit voluptatibus quaerat amet velit, labore eos corporis dolor consequuntur est iusto omnis! Sequi sint impedit quis ullam pariatur, voluptatum officiis? In recusandae velit corrupti non dolore nesciunt sequi temporibus sed eveniet iste, commodi officiis quia, dolores ullam libero aliquam sint ipsum veritatis maxime aut necessitatibus? Obcaecati architecto saepe minima magnam, odio aliquid corrupti itaque aliquam dolorum fugit sunt natus enim sit asperiores ullam. Debitis dolor numquam reiciendis tenetur perspiciatis aliquam ab ipsum, placeat nemo modi error ipsa cumque molestiae quae nulla officia id maxime consequatur alias."
-        ]
-    ];
-
-    return view('posts', [
-        'title' => "Posts",
-        'posts' => $blog_posts
-    ]);
-});
-
-
-// Single Post
-Route::get('posts/{slug}', function ($slug) {
-    $blog_posts = [
-        [
-            'title' => "Judul Post Pertama",
-            'slug' => "judul-post-pertama",
-            'author' => "Muhammad Lutfi",
-            'body' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur a illo ipsam commodi tenetur rerum cupiditate. Exercitationem, labore voluptatum ipsum cum excepturi dolor iure sint consequatur numquam libero ex laudantium sit ipsa esse nisi, dolorum expedita possimus beatae, maxime veniam a pariatur eos impedit. Incidunt mollitia laboriosam dolorum odio harum ratione, quo, quod quibusdam expedita asperiores fugit exercitationem voluptatum ex rerum neque, suscipit at eveniet corrupti repellat accusamus dicta! Sequi accusamus voluptatum voluptate, aut animi provident laboriosam molestias incidunt ullam."
-        ],
-        [
-            'title' => "Judul Post Kedua",
-            'slug' => "judul-post-kedua",
-            'author' => "Fauzi Kirito",
-            'body' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde provident aperiam ut vero odit voluptatibus quaerat amet velit, labore eos corporis dolor consequuntur est iusto omnis! Sequi sint impedit quis ullam pariatur, voluptatum officiis? In recusandae velit corrupti non dolore nesciunt sequi temporibus sed eveniet iste, commodi officiis quia, dolores ullam libero aliquam sint ipsum veritatis maxime aut necessitatibus? Obcaecati architecto saepe minima magnam, odio aliquid corrupti itaque aliquam dolorum fugit sunt natus enim sit asperiores ullam. Debitis dolor numquam reiciendis tenetur perspiciatis aliquam ab ipsum, placeat nemo modi error ipsa cumque molestiae quae nulla officia id maxime consequatur alias."
-        ]
-    ];
-
-    $new_post = [];
-
-    foreach ($blog_posts as $post) {
-        if ($post['slug'] === $slug) {
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        'title' => "Single Post",
-        'post' => $new_post
-    ]);
-});
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('posts/{slug}', [POstController::class, 'show']);

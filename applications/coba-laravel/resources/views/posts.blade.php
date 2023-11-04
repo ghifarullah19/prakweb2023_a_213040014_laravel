d5c5e2e563c352d308d9bc0039be7a21bb6532e9
Muhammad Lutfi Amin Ghifarullah <ghifarullah11@gmail.com>
2023-11-04 19:55:57 +0700

    11. N + 1 Problem

--- applications/coba-laravel/resources/views/posts.blade.php
+++ applications/coba-laravel/resources/views/posts.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1 class="mb-5">Halaman Blog</h1>
+    <h1 class="mb-5">{{ $title }}</h1>
     
     @foreach ($posts as $post)
         <article class="mb-5 border-bottom pb-4">
             <h2>
-                <a href="posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
+                <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
             </h2>
             <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
             <p>{{ $post->excerpt }}</p>
 
-            <a href="posts/{{ $post->slug }}" class="text-decoration-none">Read more..</a>
+            <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read more..</a>
         </article>
     @endforeach
 @endsection

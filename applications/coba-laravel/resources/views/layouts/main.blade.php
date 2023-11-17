9aeb90fe7dcb0917c7684bdb7936016f9a860e45
Muhammad Lutfi Amin Ghifarullah <ghifarullah11@gmail.com>
2023-11-18 01:29:57 +0700

    14. View Login & Registration

--- applications/coba-laravel/resources/views/layouts/main.blade.php
+++ applications/coba-laravel/resources/views/layouts/main.blade.php
@@ -4,7 +4,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>WPU Blog | {{ $title }}</title>
+
+    {{-- bootstrap css --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    {{-- bootstrap icons --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+
+    {{-- my style --}}
+    <link rel="stylesheet" href="/css/style.css">
   </head>
   <body>
     

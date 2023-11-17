5d0285a3d10030055d42b38dee814ca42d933690
Muhammad Lutfi Amin Ghifarullah <ghifarullah11@gmail.com>
2023-11-18 02:14:13 +0700

    15. User Registration

--- applications/coba-laravel/routes/web.php
+++ applications/coba-laravel/routes/web.php
@@ -48,4 +48,6 @@ Route::get('/categories', function () {
 });
 
 Route::get('/login', [LoginController::class, 'index']);
+
 Route::get('/register', [RegisterController::class, 'index']);
+Route::post('/register', [RegisterController::class, 'store']);
